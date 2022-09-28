ff4bcee5b7661aed91b5941ededce8bffcd6fab8
jrparente <jrparente@gmail.com>
2022-09-28 15:21:23 +0100

    Updated suggestions with JS

--- src/app.js
+++ src/app.js
@@ -4,7 +4,13 @@ let today = new Date();
 let date = today.getDate();
 let year = today.getFullYear();
 let minutes = today.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
 let hour = today.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
 
 let days = [
   "Sunday",
@@ -39,6 +45,8 @@ updateDate.innerHTML = `${day}, ${month} ${date} ${year}, ${hour}:${minutes}`;
 // Get temperature in city
 function getWeather(response) {
   console.log(response.data);
+  let cityHTML = document.querySelector("#titleCity");
+  cityHTML.innerHTML = response.data.name;
   let cityTemp = document.querySelector("#todays-temp");
   let temp = Math.round(response.data.main.temp);
   cityTemp.innerHTML = temp;
@@ -54,32 +62,49 @@ function getWeather(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   let description = document.querySelector(".highlight-text");
+  let advice = document.querySelector(".advice");
   let weatherdescription = response.data.weather[0].main;
   if (weatherdescription === "Clear") {
-    description.innerHTML = "clear";
+    description.innerHTML = "sunny";
+    advice.innerHTML = "Don't forget your sunglasses!";
   }
   if (weatherdescription === "Clouds") {
     description.innerHTML = "cloudy";
+    advice.innerHTML = "You should still apply SPF!";
   }
   if (weatherdescription === "Rain") {
     description.innerHTML = "rainy";
+    advice.innerHTML = "Don't forget your umbrella!";
   }
   if (weatherdescription === "Drizzle") {
     description.innerHTML = "drizzly";
+    advice.innerHTML = "Don't forget your umbrella!";
   }
   if (weatherdescription === "Thunderstorm") {
-    description.innerHTML = "electric";
+    description.innerHTML = "stormy";
+    advice.innerHTML = "Don't stand under a tree!";
   }
   if (weatherdescription === "Snow") {
     description.innerHTML = "snowy";
+    advice.innerHTML = "Don't forget your hat and mittens!";
+  }
+  if (weatherdescription === "Mist") {
+    description.innerHTML = "misty";
+    advice.innerHTML = "Don't forget your coat!";
+  }
+  if (weatherdescription === "Smoke") {
+    description.innerHTML = "smoky";
+    advice.innerHTML = "The air isn't healthy today. Stay in!";
+  }
+  if (weatherdescription === "Fog") {
+    description.innerHTML = "foggy";
+    advice.innerHTML = "It's not a good day to play outside";
   }
 }
 
 function getCity(event) {
   event.preventDefault();
   let city = document.querySelector("#city-input");
-  let cityHTML = document.querySelector("#titleCity");
-  cityHTML.innerHTML = city.value;
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&units=metric&appid=${apiKey}`;
   axios.get(url).then(getWeather);
 }
